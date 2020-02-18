cfaff6f567277745e72d6b7c6c5e8f495a40ec8f
Colin Lyons <Colinlyons53@gmail.com>
2020-02-17 20:45:59 -0800

    changed to a forloop to keep it simple, needs testing

--- Practice/LC125_isPalindrome/index.js
+++ Practice/LC125_isPalindrome/index.js
@@ -1,5 +1,16 @@
-function isPalindrome(s,i) {
-    return(i=i||0)<0||i>=s.length
+
+
+
+
+
+function isPalindrome(str) {
+    //rou
+    let len = math.floor(str.length / 2);
+    for (var i = 0; i < len; i++)
+        if (str[i] !== str[str.length - i - 1])
+            return false;
+        return true;
+
 }
 
 module.exports = isPalindrome;
