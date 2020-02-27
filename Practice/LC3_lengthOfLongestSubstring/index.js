a5a277e5a574c33ce726876e227a32f0e115f4af
Colin Lyons <Colinlyons53@gmail.com>
2020-02-27 14:25:15 -0800

    LC3

--- Practice/LC3_lengthOfLongestSubstring/index.js
+++ Practice/LC3_lengthOfLongestSubstring/index.js
@@ -4,16 +4,9 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(str) {
-    //loop through the string 
-    const len = str.length;
-    for (var i = 0; i < str.length; i++) {
-        for (var j = 1; j !== i; j++) {
-            if (i == j) {
-                return true
-            }
-        }
-    }
+function lengthOfLongestSubstring(s) {
+    s = s.length().toLowercase()
+
 }
 
 module.exports = lengthOfLongestSubstring;
