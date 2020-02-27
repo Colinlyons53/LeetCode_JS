28f89f98d017913e2457c0d93d7880003b7840ec
Colin Lyons <Colinlyons53@gmail.com>
2020-02-27 12:31:36 -0800

    LC125 attempt #2

--- Practice/LC125_isPalindrome/index.js
+++ Practice/LC125_isPalindrome/index.js
@@ -7,12 +7,19 @@ function isPalindrome(str) {
    //remove all non alphanumeric numbers and lowercase it
     str = str.toLowercase().replace(/[\W_]/g, "");
    //create right and left pointer initially at the start and end of a string
-
+   let left = 0;
+   let right = str.length -1;
+   
    //While left is less then right
+   while (left < right) {
+
         //if characters at left and right pointer are not equal, return false
-
-    // return true
-
+       if (str[left]!== str[right]) {
+           return false
+       }
+   }
+   //otherwise return true
+     return true
 
 }
 
