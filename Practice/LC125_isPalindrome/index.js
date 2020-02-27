4c433580a465f563aa3502beb9ba621259cdd86f
Colin Lyons <Colinlyons53@gmail.com>
2020-02-26 22:21:20 -0800

    coded first line of psuedocode LC125

--- Practice/LC125_isPalindrome/index.js
+++ Practice/LC125_isPalindrome/index.js
@@ -5,7 +5,7 @@
 
 function isPalindrome(str) {
    //remove all non alphanumeric numbers and lowercase it
-    str = str.toLowercase().replace()
+    str = str.toLowercase().replace(/[\W_]/g, "");
    //create right and left pointer initially at the start and end of a string
 
    //While left is less then right
